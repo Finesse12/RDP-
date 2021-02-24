8eb9d8ebf5fcdb84d28a429ab609f5e70f92badf
alok676875 <74966945+alok676875@users.noreply.github.com>
2021-02-24 22:53:00 +0530

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -7,7 +7,7 @@ printf "https://youtu.be/VeO22kIvZOQ" >&2
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb -P /tmp
 sudo apt install /tmp/chrome-remote-desktop_current_amd64.deb
-sudo apt install -y xfce4
+sudo apt -y install xfce4
 sudo apt -y install firefox
 sudo adduser ALOK chrome-remote-desktop
 } &> /dev/null &&
