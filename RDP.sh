2a2327fcb489e953b5735b8b4724ebcf470a027e
alok676875 <74966945+alok676875@users.noreply.github.com>
2021-02-24 22:10:00 +0530

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 printf "Check Out My Channel While Waiting- https://youtu.be/VeO22kIvZOQ" >&2
 {
 sudo useradd -m ALOK
@@ -9,12 +10,8 @@ sudo apt install /tmp/chrome-remote-desktop_current_amd64.deb
 sudo apt install -y xfce4
 sudo apt -y install firefox
 sudo adduser ALOK chrome-remote-desktop
-} &> /dev/null &&
-printf "\nSetup Completed " >&2 ||
-printf "\nError Occured " >&2
 printf '\n https://remotedesktop.google.com/headless \n'
 printf "Check Out My Channel While Waiting- https://youtu.be/VeO22kIvZOQ" >&2
-{
 read -p "Paste Here: " CRP
 su - ALOK -c """$CRP"""
 } &> /dev/null &&
