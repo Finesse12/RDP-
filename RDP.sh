89efb5f654f24fd01ee785a045468a607f161a06
alok676875 <74966945+alok676875@users.noreply.github.com>
2021-02-24 22:11:54 +0530

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,6 +1,3 @@
-#! /bin/bash
-printf "Check Out My Channel While Waiting- https://youtu.be/VeO22kIvZOQ" >&2
-{
 sudo useradd -m ALOK
 sudo adduser ALOK sudo
 sudo passwd
@@ -14,7 +11,3 @@ printf '\n https://remotedesktop.google.com/headless \n'
 printf "Check Out My Channel While Waiting- https://youtu.be/VeO22kIvZOQ" >&2
 read -p "Paste Here: " CRP
 su - ALOK -c """$CRP"""
-} &> /dev/null &&
-printf "\nSetup Completed " >&2 ||
-printf "\nError Occured " >&2
-printf 'Check https://remotedesktop.google.com/access/ '
