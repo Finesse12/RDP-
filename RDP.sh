ba72ff27ce5421182ade17d778c53a0e099bd245
alok676875 <74966945+alok676875@users.noreply.github.com>
2021-02-25 22:51:09 +0530

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -9,6 +9,10 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb 
 sudo apt -y install /tmp/chrome-remote-desktop_current_amd64.deb
 sudo apt -y install xfce4
 sudo apt -y install firefox
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
+apt update
+apt -y install anydesk
 sudo adduser ALOK chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Completed " >&2 ||
